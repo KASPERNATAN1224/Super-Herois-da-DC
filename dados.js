a943d838b592bd611d26c0f14b17bb563e6b2bd7
KASPERNATAN1224 <michaekasper@gmail.com>
2024-09-10 21:49:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados =[
+    {
+        titulo : "Batman",
+        descricao : "O Batman é mais do que apenas um vigilante mascarado. Ele representa um símbolo de esperança para os cidadãos de Gotham City, uma figura que luta incansavelmente para proteger os inocentes e combater a corrupção.",
+                        link : "https://pt.wikipedia.org/wiki/Batman",
+                        tag: "vigilante Gotham mascarado"
+    },
+    {
+        titulo : "Superman",
+        descricao : "A história do Superman é uma jornada épica que transcende gerações. É a história de um alienígena que se tornou um herói, um símbolo de esperança em um mundo incerto. Seus valores e ideais continuam relevantes até hoje, tornando-o um personagem atemporal e universal.",
+                        link : "https://pt.wikipedia.org/wiki/Superman",
+                        tag : "alienigena esperança universal"
+    },
+    {
+        titulo : "Wonder Woman",
+        descricao : "A Mulher Maravilha é mais do que apenas uma super-heroína. Ela é um símbolo de esperança, força e empoderamento feminino. Sua história transcende gerações e continua a inspirar milhões de pessoas ao redor do mundo.",
+                        link : "https://pt.wikipedia.org/wiki/Mulher-Maravilha",
+                        tag :  "heroina empoderamento feminino"
+    },
+    {
+        titulo : "Flash",
+        descricao : "O Flash é um dos personagens mais populares e icônicos da DC Comics. Sua história é uma jornada emocionante que explora temas como tempo, destino e a natureza da realidade. Com sua velocidade e positividade, o Flash continua a inspirar fãs de todas as idades.",
+                        link : "https://pt.wikipedia.org/wiki/Flash_(DC_Comics)",
+                        tag : "tempo destino natureza"
+    },
+    {
+        titulo : "Cyborg",
+        descricao : "A história de Cyborg é uma jornada de superação e aceitação. Ele é um personagem que nos faz refletir sobre os limites da tecnologia e a importância da humanidade em um mundo cada vez mais conectado. Cyborg é um símbolo de esperança, mostrando que mesmo diante da adversidade, é possível encontrar força e propósito.",
+                        link : "https://pt.wikipedia.org/wiki/Cyborg_(DC_Comics)",
+                        tag : "superação aceitação força"
+    },
+];
+
